17cb2e74356381a823fee0b95a6a4c0be77e5175
RaniaMostafa55 <raniamostafah55@gmail.com>
2025-01-08 23:06:40 +0200

    create customers provider

--- lib/app/modules/customers/providers/customers_provider.dart
+++ lib/app/modules/customers/providers/customers_provider.dart
@@ -2,16 +2,62 @@ import 'package:get/get.dart';
 import 'package:perfect_elegance/app/data/providers/api_provider.dart';
 
 class CustomerProvider extends ApiProvider {
-  // NOTE :- Post Login
-  Future<Map<String, dynamic>?> postLogin(
-      {required String email,
-      required String password,
-      required String deviceToken}) async {
+  // NOTE :- GET Customers
+  Future<Map<String, dynamic>?> getCustomers() async {
+    Response<Map<String, dynamic>?> res = await get<Map<String, dynamic>?>(
+        'customers',
+        headers: {'Accept': 'application/json'});
+    if (res.status.isServerError || res.status.connectionError) {
+      return null;
+    } else {
+      Get.log(res.statusCode.toString());
+      return {
+        'code': res.statusCode!,
+        'data': res.body as Map<String, dynamic>,
+      };
+    }
+  }
+
+  // NOTE :- GET Search For Customers
+  Future<Map<String, dynamic>?> getSearchForCustomers() async {
+    Response<Map<String, dynamic>?> res = await get<Map<String, dynamic>?>(
+        'customers/search',
+        headers: {'Accept': 'application/json'});
+    if (res.status.isServerError || res.status.connectionError) {
+      return null;
+    } else {
+      Get.log(res.statusCode.toString());
+      return {
+        'code': res.statusCode!,
+        'data': res.body as Map<String, dynamic>,
+      };
+    }
+  }
+
+  // NOTE :- Post New Customer
+  Future<Map<String, dynamic>?> postNewCustomer(
+      {required String name,
+      required String email,
+      required String phone,
+      required String phone2,
+      required String address,
+      required String cityId,
+      required String cityName,
+      required String socialType,
+      required String socialLink,
+      required String levelId}) async {
     Response<Map<String, dynamic>?> res =
-        await post<Map<String, dynamic>?>('api/v1/auth', {
+        await post<Map<String, dynamic>?>('customers/store', {
+      'name': name,
       'email': email,
-      'password': password,
-      'device_token': deviceToken,
+      'phone': phone,
+      'phone2': phone2,
+      'address': address,
+      'city_id': cityId,
+      'city_name': cityName,
+      'social_type': socialType,
+      'social_link': socialLink,
+      'level_id': levelId
     }, headers: {
       'Accept': 'application/json'
     });
@@ -26,101 +72,101 @@ class CustomerProvider extends ApiProvider {
     }
   }
 
-  // NOTE :- Post Login
-  Future<Map<String, dynamic>?> postStoreSignup({
-    required String name,
-    required String managerName,
-    required String phone,
-    required String address,
-    required String storelink,
-    required String passportNumber,
-    required String idNumber,
-    required String license,
-    required String email,
-    required String password,
-  }) async {
-    Response<Map<String, dynamic>?> res = await post<Map<String, dynamic>?>(
-      'api/v1/store-signup',
-      {
-        'name': name,
-        'manager_name': managerName,
-        'phone': phone,
-        'address': address,
-        'storelink': storelink,
-        'passport_number': passportNumber,
-        'id_number': idNumber,
-        'license': license,
-        'email': email,
-        'password': password,
-      },
-      headers: {'Accept': 'application/json'},
-    );
-    if (res.status.isServerError || res.status.connectionError) {
-      return null;
-    } else {
-      Get.log(res.statusCode.toString());
-      return {
-        'code': res.statusCode!,
-        'data': res.body as Map<String, dynamic>,
-      };
-    }
-  }
+  // // NOTE :- Post Login
+  // Future<Map<String, dynamic>?> postStoreSignup({
+  //   required String name,
+  //   required String managerName,
+  //   required String phone,
+  //   required String address,
+  //   required String storelink,
+  //   required String passportNumber,
+  //   required String idNumber,
+  //   required String license,
+  //   required String email,
+  //   required String password,
+  // }) async {
+  //   Response<Map<String, dynamic>?> res = await post<Map<String, dynamic>?>(
+  //     'api/v1/store-signup',
+  //     {
+  //       'name': name,
+  //       'manager_name': managerName,
+  //       'phone': phone,
+  //       'address': address,
+  //       'storelink': storelink,
+  //       'passport_number': passportNumber,
+  //       'id_number': idNumber,
+  //       'license': license,
+  //       'email': email,
+  //       'password': password,
+  //     },
+  //     headers: {'Accept': 'application/json'},
+  //   );
+  //   if (res.status.isServerError || res.status.connectionError) {
+  //     return null;
+  //   } else {
+  //     Get.log(res.statusCode.toString());
+  //     return {
+  //       'code': res.statusCode!,
+  //       'data': res.body as Map<String, dynamic>,
+  //     };
+  //   }
+  // }
 
-  // NOTE :- Post Reset Pass OTP
-  Future<int?> postResetPassOtp({required String email}) async {
-    Response<Map<String, dynamic>?> res = await post<Map<String, dynamic>?>(
-      'api/v1/forget-password',
-      {
-        'email': email,
-      },
-      headers: {'Accept': 'application/json'},
-    );
-    if (res.status.isServerError || res.status.connectionError) {
-      return null;
-    } else {
-      Get.log(res.statusCode.toString());
-      return res.statusCode!;
-    }
-  }
+  // // NOTE :- Post Reset Pass OTP
+  // Future<int?> postResetPassOtp({required String email}) async {
+  //   Response<Map<String, dynamic>?> res = await post<Map<String, dynamic>?>(
+  //     'api/v1/forget-password',
+  //     {
+  //       'email': email,
+  //     },
+  //     headers: {'Accept': 'application/json'},
+  //   );
+  //   if (res.status.isServerError || res.status.connectionError) {
+  //     return null;
+  //   } else {
+  //     Get.log(res.statusCode.toString());
+  //     return res.statusCode!;
+  //   }
+  // }
 
-  // NOTE :- Post Verify Pass OTP
-  Future<int?> postVerifyOtp({
-    required String email,
-    required String otp,
-  }) async {
-    Response res = await post(
-      'api/v1/verify-otp',
-      {'email': email, 'otp': otp},
-      headers: {'Accept': 'application/json'},
-    );
-    if (res.status.isServerError || res.status.connectionError) {
-      return null;
-    } else {
-      Get.log(res.statusCode.toString());
-      return res.statusCode!;
-    }
-  }
+  // // NOTE :- Post Verify Pass OTP
+  // Future<int?> postVerifyOtp({
+  //   required String email,
+  //   required String otp,
+  // }) async {
+  //   Response res = await post(
+  //     'api/v1/verify-otp',
+  //     {'email': email, 'otp': otp},
+  //     headers: {'Accept': 'application/json'},
+  //   );
+  //   if (res.status.isServerError || res.status.connectionError) {
+  //     return null;
+  //   } else {
+  //     Get.log(res.statusCode.toString());
+  //     return res.statusCode!;
+  //   }
+  // }
 
-  // NOTE :- Post Reset Pass
-  Future<int?> postResetPass({
-    required String email,
-    required String password,
-    required String confirmPassword,
-    required String token,
-  }) async {
-    Response res = await post('api/v1/update-password', {
-      'email': email,
-      'password': password,
-      'password_confirmation': confirmPassword,
-      'token': token,
-    }, headers: {
-      'Accept': 'application/json'
-    });
-    if (res.status.isServerError || res.status.connectionError) {
-      return null;
-    } else {
-      Get.log(res.statusCode.toString());
-      return res.statusCode!;
-    }
-  }
+  // // NOTE :- Post Reset Pass
+  // Future<int?> postResetPass({
+  //   required String email,
+  //   required String password,
+  //   required String confirmPassword,
+  //   required String token,
+  // }) async {
+  //   Response res = await post('api/v1/update-password', {
+  //     'email': email,
+  //     'password': password,
+  //     'password_confirmation': confirmPassword,
+  //     'token': token,
+  //   }, headers: {
+  //     'Accept': 'application/json'
+  //   });
+  //   if (res.status.isServerError || res.status.connectionError) {
+  //     return null;
+  //   } else {
+  //     Get.log(res.statusCode.toString());
+  //     return res.statusCode!;
+  //   }
+  // }
 }
