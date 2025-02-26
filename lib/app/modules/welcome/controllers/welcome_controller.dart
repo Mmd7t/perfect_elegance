f904d388b05eaf3b8ce7ec5d32c4beee7042f427
Mmd7t22 <medhat@tezda.com>
2025-02-26 21:32:55 +0200

    remove unused files and configurations for macOS, Linux, and Windows platforms

--- lib/app/modules/welcome/controllers/welcome_controller.dart
+++ lib/app/modules/welcome/controllers/welcome_controller.dart
@@ -1,4 +1,4 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
+// import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get/get.dart';
 import 'package:perfect_elegance/app/core/theme/theme.dart';
 import 'package:perfect_elegance/app/data/services/app_services.dart';
@@ -25,29 +25,29 @@ class WelcomeController extends GetxController {
   }
 
   checkToken() async {
-    final List<ConnectivityResult> connectivityResult =
-        await (Connectivity().checkConnectivity());
+    // final List<ConnectivityResult> connectivityResult =
+    //     await (Connectivity().checkConnectivity());
 
-    if (connectivityResult.contains(ConnectivityResult.none)) {
-      Get.offNamed(Routes.noInternet);
+    // if (connectivityResult.contains(ConnectivityResult.none)) {
+    //   Get.offNamed(Routes.noInternet);
+    // } else {
+    if (appServices.isFirstTime.value) {
+      Get.offNamed(Routes.onboarding);
     } else {
-      if (appServices.isFirstTime.value) {
-        Get.offNamed(Routes.onboarding);
-      } else {
-        final Map<String, dynamic>? res = await provider.getUser();
-        if (res != null) {
-          if (res['code'] == 200) {
-            appServices.saveUserName(res['data']['data']['name']);
-            appServices.saveUserEmail(res['data']['data']['email']);
-            appServices.saveUserPhone(res['data']['data']['phone']);
-            Get.offNamed(Routes.main);
-          } else {
-            Get.offNamed(Routes.login);
-          }
+      final Map<String, dynamic>? res = await provider.getUser();
+      if (res != null) {
+        if (res['code'] == 200) {
+          appServices.saveUserName(res['data']['data']['name']);
+          appServices.saveUserEmail(res['data']['data']['email']);
+          appServices.saveUserPhone(res['data']['data']['phone']);
+          Get.offNamed(Routes.main);
         } else {
-          Ui.errorGetBar(message: "خطأ في الخادم");
+          Get.offNamed(Routes.login);
         }
+      } else {
+        Ui.errorGetBar(message: "خطأ في الخادم");
       }
     }
+    // }
   }
 }
--- lib/no_internet_middleware.dart
+++ lib/no_internet_middleware.dart
@@ -1,16 +1,16 @@
-import 'package:get/get.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
+// import 'package:get/get.dart';
+// import 'package:connectivity_plus/connectivity_plus.dart';
 
-class InternetCheckMiddleware extends GetMiddleware {
-  @override
-  Future<GetNavConfig?> redirectDelegate(GetNavConfig route) async {
-    final connectivity = Connectivity();
-    final status = await connectivity.checkConnectivity();
+// class InternetCheckMiddleware extends GetMiddleware {
+//   @override
+//   Future<GetNavConfig?> redirectDelegate(GetNavConfig route) async {
+//     final connectivity = Connectivity();
+//     final status = await connectivity.checkConnectivity();
 
-    if (status.contains(ConnectivityResult.none)) {
-      return GetNavConfig.fromRoute('/no-internet');
-    }
+//     if (status.contains(ConnectivityResult.none)) {
+//       return GetNavConfig.fromRoute('/no-internet');
+//     }
 
-    return super.redirectDelegate(route);
-  }
-}
+//     return super.redirectDelegate(route);
+//   }
+// }
